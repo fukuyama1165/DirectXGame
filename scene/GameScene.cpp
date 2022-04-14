a0789267b902c341dda3d3a82fa26bb8210cc78a
k021g1165@g.neec.ac.jp <k021g1165@g.neec.ac.jp>
2022-04-14 14:59:12 +0900

    3Dオブジェクトを座標指定や角度変更、大きさ変更をできるように

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -6,7 +6,10 @@ using namespace DirectX;
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() 
+{ 
+	delete model_; 
+}
 
 void GameScene::Initialize() {
 
@@ -14,9 +17,46 @@ void GameScene::Initialize() {
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
 	debugText_ = DebugText::GetInstance();
+
+	//ファイル名を指定してテクスチャを読み込む
+	textureHandle_ = TextureManager::Load("mario.jpg");
+
+	//3Dモデルの生成
+	model_ = Model::Create();
+
+	// X,Y,Z方向のスケーリングを設定
+	worldTransform_.scale_ = {5.0f, 5.0f, 5.0f};
+
+	// X,Y,Z軸周りの回転角を設定
+	//XM_PI/4.0fはラジアンの計算 XMConvertToRadians(45.0f)で度数法で書ける
+	worldTransform_.rotation_ = {XM_PI / 4.0f, XM_PI / 4.0f, 0.0f};
+
+	// X,Y,Z軸周りの平行移動を設定
+	worldTransform_.translation_ = {10.0f, 10.0f, 10.0f};
+
+	//ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+
+	//ビュープロジェクションの初期化
+	viewProjection_.Initialize();
+
+
 }
 
-void GameScene::Update() {}
+void GameScene::Update() 
+{ 
+	debugText_->SetPos(50, 50);
+
+	debugText_->Printf("translation:(%lf,%f,%f)", worldTransform_.translation_.x, worldTransform_.translation_.y,worldTransform_.translation_.z); 
+
+	debugText_->SetPos(50, 65);
+
+	debugText_->Printf("rotation:(%lf,%f,%f)", worldTransform_.rotation_.x, worldTransform_.rotation_.y,worldTransform_.rotation_.z); 
+
+	debugText_->SetPos(50, 80);
+
+	debugText_->Printf("scale:(%lf,%f,%f)", worldTransform_.scale_.x, worldTransform_.scale_.y,worldTransform_.scale_.z);
+}
 
 void GameScene::Draw() {
 
@@ -44,6 +84,9 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
+
+	//3Dモデル描画
+	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
--- scene/GameScene.h
+++ scene/GameScene.h
@@ -51,4 +51,17 @@ class GameScene {
 	/// <summary>
 	/// ゲームシーン用
 	/// </summary>
+	
+	//テクスチャハンドル
+	uint32_t textureHandle_ = 0;
+
+	//3Dモデル
+	Model* model_ = nullptr;
+	 
+	//ワールドトランスフォーム
+	WorldTransform worldTransform_;
+
+	//ビュープロジェクション
+	ViewProjection viewProjection_;
+
 };
